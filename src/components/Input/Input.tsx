f5557ee199b2eb38a02d06b8241b27869eeb7ee4
Yeongjun Kim <kyjun519@gmail.com>
2023-10-29 15:47:29 +0900

    feat: #10 - Input 컴포넌트 구현

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,67 @@
+import { cls } from '@/utils'
+
+interface Input {
+  label?: string
+  type?: string
+  placeholder?: string
+  inputButton?: boolean
+  buttonText?: string
+  validation?: string
+  disabled?: boolean
+  multiLine?: boolean
+  onbuttonClick?: VoidFunction
+}
+
+const Input = ({
+  label,
+  type,
+  placeholder,
+  inputButton,
+  buttonText = '추가',
+  validation,
+  disabled,
+  multiLine,
+  onbuttonClick,
+}: Input) => {
+  return (
+    <div className="flex flex-col  justify-center">
+      {label && (
+        <label className="py-2 text-sm font-semibold leading-5 text-gray-900">
+          {label}
+        </label>
+      )}
+      {multiLine ? (
+        <textarea
+          className="h-16 rounded-md border border-gray-500 bg-white px-3 py-2 text-sm font-medium leading-5 text-gray-900 placeholder-gray-400 outline-none disabled:border-gray-300 disabled:placeholder-gray-300"
+          placeholder={placeholder}
+          disabled={disabled}></textarea>
+      ) : (
+        <div className="relative flex flex-col">
+          <input
+            type={type}
+            className={cls(
+              'rounded-md border border-gray-500 bg-white px-3  py-2 text-sm font-medium leading-5 text-gray-900 placeholder-gray-400 outline-none disabled:border-gray-300 disabled:placeholder-gray-300',
+              inputButton && 'border-emerald-600 pr-20',
+            )}
+            placeholder={placeholder}
+            disabled={disabled}
+          />
+          {inputButton && (
+            <button
+              className="absolute right-0 top-0 flex rounded-r-md border border-emerald-600 bg-emerald-500 px-4 py-2 text-sm font-semibold leading-5 text-white"
+              onClick={onbuttonClick}>
+              {buttonText}
+            </button>
+          )}
+        </div>
+      )}
+      {validation && (
+        <span className="py-2 text-xs font-normal leading-4 text-red-600">
+          {validation}
+        </span>
+      )}
+    </div>
+  )
+}
+
+export default Input
