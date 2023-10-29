fdab7009859eff08750e34d8c152022e0df5c66b
Yeongjun Kim <kyjun519@gmail.com>
2023-10-29 21:13:03 +0900

    style: #11 - AvatarGroup 여백 값 수정

--- src/components/AvatarGroup/constants/index.ts
+++ src/components/AvatarGroup/constants/index.ts
@@ -1,4 +1,4 @@
-export const MARGIN_LEFT_DIVIDE = 8
-export const PADDING_LEFT_DIVIDE = 5
+export const MARGIN_LEFT_DIVIDE = 7
+export const PADDING_LEFT_DIVIDE = 7
 export const DEFAULT_SIZE = 60
 export const DEFAULT_LIMIT = 3
