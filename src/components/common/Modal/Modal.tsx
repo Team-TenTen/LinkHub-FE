2d49fea2727a351f0716a000aa29e952bb4874d0
Yeongjun Kim <kyjun519@gmail.com>
2023-10-31 00:39:07 +0900

    feat: #12 - Modal 컴포넌트 스타일 수정 및 props 추가

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -1,22 +1,23 @@
 'use client'
 
-import { useState } from 'react'
 import { createPortal } from 'react-dom'
 
 interface ModalProps {
   title: string
-  children: React.ReactNode
   isClose?: boolean
-  onClose?: VoidFunction
-  onConfirm?: VoidFunction
+  isConfirm?: boolean
+  onClose?: (e?: React.MouseEvent<HTMLButtonElement>) => void
+  onConfirm?: (e?: React.MouseEvent<HTMLButtonElement>) => void
+  children: React.ReactNode
 }
 
 const Modal = ({
   title,
-  children,
   isClose,
+  isConfirm,
   onClose,
   onConfirm,
+  children,
 }: ModalProps) => {
   const rootNode = document.getElementById('root')
 
@@ -25,25 +26,29 @@ const Modal = ({
       {rootNode &&
         createPortal(
           <div className="fixed left-0 right-0 top-0 mx-auto flex h-screen w-full max-w-[500px] flex-col justify-center bg-black/40 px-4">
-            <div className="flex w-full flex-col gap-2 rounded-xl border border-gray-300 bg-white p-4 shadow-md">
-              <div className="text-base font-semibold text-gray-900">
-                {title}
+            <div className="border-gray3 flex w-full flex-col gap-2 rounded-xl border bg-bgColor p-4 shadow-md">
+              <div className="flex items-center justify-between">
+                <div className="text-gray9 text-base font-semibold">
+                  {title}
+                </div>
+                <button onClick={onClose}>X</button>
               </div>
               {children}
               <div className="flex justify-end gap-2">
                 {isClose && (
                   <button
-                    className="flex items-center justify-center rounded-md border border-gray-500 bg-gray-400 px-2 py-1 text-sm font-bold text-white"
+                    className="border-slate5 bg-slate4 flex items-center justify-center rounded-md border px-2.5 py-1.5 text-sm font-bold text-white"
                     onClick={onClose}>
                     취소
                   </button>
                 )}
-
-                <button
-                  className="flex items-center justify-center rounded-md border border-emerald-600 bg-emerald-500 px-2 py-1 text-sm font-bold text-white"
-                  onClick={onConfirm}>
-                  추가
-                </button>
+                {isConfirm && (
+                  <button
+                    className="border-emerald6 bg-emerald5 flex items-center justify-center rounded-md border px-2.5 py-1.5 text-sm font-bold text-white"
+                    onClick={onConfirm}>
+                    추가
+                  </button>
+                )}
               </div>
             </div>
           </div>,
