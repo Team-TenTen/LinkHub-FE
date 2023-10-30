98fcd6aa606e27505295f835a20874f3040218b8
권기범 <bomi@gwongibeom-ui-MacBookAir.local>
2023-10-30 17:31:41 +0900

    feature #7 - 버튼 컴포넌트 global css에 변수화

--- src/app/(routes)/bomiTest/page.tsx
+++ src/app/(routes)/bomiTest/page.tsx
@@ -1,17 +1,59 @@
 // 컴포넌트 테스트 코드입니다
 import { Button } from '@/components'
+import { PencilSquareIcon } from '@heroicons/react/24/outline'
+import { HeartIcon } from '@heroicons/react/24/outline'
+import { PlusSmallIcon } from '@heroicons/react/24/solid'
 
 const TestPage = () => {
   return (
     <>
-      <div>
+      <div className="m-1">
         <Button className="button button-md button-white">흰색 버튼</Button>
       </div>
-      <div>
+      <div className="m-1">
         <Button className="button button-md button-emerald">초록 버튼</Button>
       </div>
-      <div>
+      <div className="m-1">
         <Button className="button button-md button-gray">회색 버튼</Button>
+      </div>
+      <div className="m-1">
+        <Button className="button button-md button-lg button-white">
+          흰색 큰 버튼
+        </Button>
+      </div>
+      <div className="m-1">
+        <Button className="button button-md button-lg button-emerald">
+          초록 큰 버튼
+        </Button>
+      </div>
+      <div className="m-1">
+        <Button className="button button-md button-lg button-gray">
+          회색 큰 버튼
+        </Button>
+      </div>
+      <div className="m-1">
+        <Button className="button button-sm button-emerald">
+          <PlusSmallIcon className="h-5 w-5" />
+        </Button>
+      </div>
+      <div className="m-1">
+        <Button className="button button-sm button-gray">
+          <PlusSmallIcon className="h-5 w-5" />
+        </Button>
+      </div>
+      <div className="m-1">
+        <Button className="button button-sm button-white">
+          <PencilSquareIcon className="h-5 w-5" />
+        </Button>
+      </div>
+      <div className="m-1">
+        <Button className="button button-round button-white">Button</Button>
+      </div>
+      <div className="m-1">
+        <Button className="button button-round button-white">
+          <HeartIcon className="h-4 w-4" />
+          Button
+        </Button>
       </div>
     </>
   )
