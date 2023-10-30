5691792a67b20add79a1d3c24d534c8c4b1b8a9a
Yeongjun Kim <kyjun519@gmail.com>
2023-10-30 20:27:52 +0900

    refactor: #12 - common 폴더 내부로 파일 이동

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
 export { default as Providers } from './Providers/providers'
 export { default as ThemeButton } from './ThemeButton/themeButton'
-export { default as Modal } from './Modal/Modal'
+export { default as Modal } from './common/Modal/Modal'
