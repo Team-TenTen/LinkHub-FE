17ae6d6d10f483a0e326d327b5462b5c4fc0da96
권기범 <bomi@gwongibeom-ui-MacBookAir.local>
2023-10-31 17:11:01 +0900

    feat: #9 - 스페이스 컴포넌트 구현

---
+++ src/app/(routes)/bomiTest/spaceTest/page.tsx
@@ -0,0 +1,41 @@
+import Space from '@/components/common/Space/Space'
+
+const TestPage = () => {
+  const spaceObj = {
+    spaceImage: '/TestImage.svg',
+    spaceName: '강남역 맛집 리스트 모음 스페이스',
+    description: '내 기준 강남역에서 맛있는 맛집 링크 모음집',
+    category: '생활•노하우•쇼핑',
+    favorite: 60,
+    scrap: 40,
+  }
+
+  return (
+    <div>
+      <div className="m-4">
+        <Space
+          type="Card"
+          spaceName={spaceObj.spaceName}
+          spaceImage={spaceObj.spaceImage}
+          description={spaceObj.description}
+          category={spaceObj.category}
+          scrap={spaceObj.scrap}
+          favorite={spaceObj.favorite}
+        />
+      </div>
+      <div>
+        <Space
+          type="Header"
+          spaceName={spaceObj.spaceName}
+          spaceImage={spaceObj.spaceImage}
+          description={spaceObj.description}
+          category={spaceObj.category}
+          scrap={spaceObj.scrap}
+          favorite={spaceObj.favorite}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default TestPage
--- src/components/common/Space/Space.tsx
+++ src/components/common/Space/Space.tsx
@@ -1,12 +1,28 @@
+import { StarIcon as StarIconOutline } from '@heroicons/react/24/outline'
 import { InboxArrowDownIcon } from '@heroicons/react/24/solid'
-import { StarIcon } from '@heroicons/react/24/solid'
+import { StarIcon as StarIconSolid } from '@heroicons/react/24/solid'
 import Image from 'next/image'
+import Chip from '../Chip/Chip'
 
 interface SpaceProps {
   type: 'Card' | 'Header'
+  spaceName: string
+  spaceImage: string
+  description: string
+  category: string
+  scrap: number
+  favorite: number
 }
 
-const Space = ({ type }: SpaceProps) => {
+const Space = ({
+  type,
+  spaceName,
+  spaceImage,
+  description,
+  category,
+  scrap,
+  favorite,
+}: SpaceProps) => {
   return (
     <>
       {type === 'Card' ? (
@@ -14,71 +30,70 @@ const Space = ({ type }: SpaceProps) => {
           <div className="items-center justify-center">
             <Image
               className="h-20 w-20 rounded-full border border-slate-300 object-cover"
-              src="/TestImage.svg"
+              src={spaceImage}
               width={80}
               height={80}
               alt="space-image"
             />
           </div>
           <div className="flex grow flex-col justify-center gap-1">
-            <div className="text-sm font-bold text-gray-900">SpaceName</div>
-            <div className="text-xs font-normal text-gray-600">Description</div>
+            <div className="text-sm font-bold text-gray9">{spaceName}</div>
+            <div className="text-xs font-normal text-gray6">{description}</div>
             <div className="flex items-center justify-end">
-              <div className="rounded-xl bg-emerald-200 px-2.5 py-1 text-xs font-medium text-emerald-900">
-                카테고리
-              </div>
+              <Chip label={category} />
               <div className="flex grow items-center justify-end gap-2">
                 <button
                   className="
-                flex items-center justify-center gap-1 rounded-xl border border-slate6 bg-bgColor px-1.5 py-1
-                text-xs font-medium text-slate6
-              ">
+                    flex items-center justify-center gap-1 rounded-xl border border-slate6 bg-bgColor px-1.5 py-1
+                    text-xs font-medium text-slate6
+                  ">
                   <InboxArrowDownIcon className="h-4 w-4" />
-                  가져오기
+                  가져오기 {scrap}
                 </button>
                 <button
                   className="
-                flex items-center justify-center gap-1 rounded-xl border border-slate6 bg-bgColor px-1.5 py-1
-                text-xs font-medium text-slate6
-              ">
-                  <StarIcon className="h-4 w-4 text-yellow-300" />
-                  즐겨찾기
+                    flex items-center justify-center gap-1 rounded-xl border border-slate6 bg-bgColor px-1.5 py-1
+                    text-xs font-medium text-slate6
+                  ">
+                  <StarIconOutline className="h-4 w-4" />
+                  즐겨찾기 {favorite}
                 </button>
               </div>
             </div>
           </div>
         </div>
       ) : (
-        <div className="px-3 py-1.5">
-          <div>
-            <Image
-              className="w-[500px]"
-              src="TestImage.svg"
-              width={500}
-              height={188}
-              alt="space-image"
-            />
-            <div>category</div>
-            <div>SpaceName</div>
-            <div>
+        <div className="relative flex flex-col gap-10 p-4">
+          <Image
+            className="z-[-100] h-[188px] object-cover"
+            src={spaceImage}
+            alt="space-image"
+            fill
+          />
+          <div className="flex justify-end">
+            <Chip label={category} />
+          </div>
+          <div className="flex flex-col gap-1.5 rounded-md bg-white bg-opacity-60 px-3 py-1.5 dark:bg-gray-900 dark:bg-opacity-60">
+            <div className="text-lg font-bold text-gray9">{spaceName}</div>
+            <div className="flex grow items-center gap-2">
               <button
                 className="
-                flex items-center justify-center gap-1 rounded-xl border border-slate6 bg-bgColor px-1.5 py-1
-                text-xs font-medium text-slate6
-              ">
+                    flex items-center justify-center gap-1 rounded-xl border border-slate6 bg-bgColor px-1.5 py-1
+                    text-xs font-medium text-slate6
+                  ">
                 <InboxArrowDownIcon className="h-4 w-4" />
-                가져오기
+                가져오기 {scrap}
               </button>
               <button
                 className="
-                flex items-center justify-center gap-1 rounded-xl border border-slate6 bg-bgColor px-1.5 py-1
-                text-xs font-medium text-slate6
-              ">
-                <StarIcon className="h-4 w-4 text-yellow-300" />
-                즐겨찾기
+                    flex items-center justify-center gap-1 rounded-xl border border-slate6 bg-bgColor px-1.5 py-1
+                    text-xs font-medium text-slate6
+                  ">
+                <StarIconSolid className="h-4 w-4 text-yellow-300" />
+                즐겨찾기 {favorite}
               </button>
             </div>
-            <div>Description</div>
+            <div className="text-xs font-normal text-gray6">{description}</div>
           </div>
         </div>
       )}
