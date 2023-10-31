1c9c162b8fd100b7d4f895918ae27efa2c0bfd00
Yeongjun Kim <kyjun519@gmail.com>
2023-10-31 16:20:36 +0900

    refactor: #27 - Providers 파일명 수정 완료

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-export { default as Providers } from './Providers/providers'
+export { default as Providers } from './Providers/Providers'
 export { default as ThemeButton } from './ThemeButton/themeButton'
 export { default as Avatar } from './common/Avatar/Avatar'
 export { default as AvatarGroup } from './common/AvatarGroup/AvatarGroup'
