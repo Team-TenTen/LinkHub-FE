2e90db63bf18f5f873f9090c5a48b27433d4a407
Yeongjun Kim <kyjun519@gmail.com>
2023-10-31 19:10:07 +0900

    feat: #12 - Modal 컴포넌트 props 수정 및 useModalLoggic hook 추가

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -1,51 +1,65 @@
 'use client'
 
+import { useRef } from 'react'
 import { createPortal } from 'react-dom'
+import { XMarkIcon } from '@heroicons/react/24/solid'
+import useModalLogic from './hooks/useModalLogic'
 
 export interface ModalProps {
   title: string
-  isClose?: boolean
-  isConfirm?: boolean
-  onClose?: (e?: React.MouseEvent<HTMLButtonElement>) => void
+  isCancelButton?: boolean
+  isConfirmButton?: boolean
+  onClose: (e?: React.MouseEvent<HTMLButtonElement>) => void
   onConfirm?: (e?: React.MouseEvent<HTMLButtonElement>) => void
   children: React.ReactNode
 }
 
 const Modal = ({
   title,
-  isClose,
-  isConfirm,
+  isCancelButton,
+  isConfirmButton,
   onClose,
   onConfirm,
   children,
 }: ModalProps) => {
   const rootNode = document.getElementById('root')
+  const modalRef = useRef<HTMLDivElement | null>(null)
+  const { handleClickOverlay, handleClickConfirm } = useModalLogic({
+    onClose,
+    onConfirm,
+    modalRef,
+  })
 
   return (
     <>
       {rootNode &&
         createPortal(
-          <div className="fixed left-0 right-0 top-0 mx-auto flex h-screen w-full max-w-[500px] flex-col justify-center bg-black/40 px-4">
-            <div className="border-gray3 flex w-full flex-col gap-2 rounded-xl border bg-bgColor p-4 shadow-md">
+          <div
+            ref={modalRef}
+            onClick={handleClickOverlay}
+            className="fixed left-0 right-0 top-0 mx-auto flex h-screen w-full max-w-[500px] flex-col justify-center bg-black/40 px-4">
+            <div className="flex w-full flex-col gap-2 rounded-xl border border-gray3 bg-bgColor p-4 shadow-md">
               <div className="flex items-center justify-between">
-                <div className="text-gray9 text-base font-semibold">
+                <div className="text-base font-semibold text-gray9">
                   {title}
                 </div>
-                <button onClick={onClose}>X</button>
+                <button onClick={onClose}>
+                  <XMarkIcon className="h-4 w-4" />
+                </button>
               </div>
               {children}
               <div className="flex justify-end gap-2">
-                {isClose && (
+                {isCancelButton && (
                   <button
-                    className="border-slate5 bg-slate4 flex items-center justify-center rounded-md border px-2.5 py-1.5 text-sm font-bold text-white"
+                    className="flex items-center justify-center rounded-md border border-slate5 bg-slate4 px-2.5 py-1.5 text-sm font-bold text-white"
                     onClick={onClose}>
                     취소
                   </button>
                 )}
-                {isConfirm && (
+                {isConfirmButton && (
                   <button
-                    className="border-emerald6 bg-emerald5 flex items-center justify-center rounded-md border px-2.5 py-1.5 text-sm font-bold text-white"
-                    onClick={onConfirm}>
+                    className="flex items-center justify-center rounded-md border border-emerald6 bg-emerald5 px-2.5 py-1.5 text-sm font-bold text-white"
+                    onClick={handleClickConfirm}>
                     추가
                   </button>
                 )}
