7f4bdf9754bf201f3a40f741fbedfbf1fc2ef835
Yeongjun Kim <kyjun519@gmail.com>
2023-10-31 16:19:27 +0900

    refactor: #27 - Providers 파일명 수정 진행

--- src/components/Providers/providers.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { ThemeProvider } from 'next-themes'
-
-const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>
-}
-
-export default Providers
