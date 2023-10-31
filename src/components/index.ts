ba4e58b453720b0aee04b9e96d84cf0dd0e99cd0
Yeongjun Kim <kyjun519@gmail.com>
2023-10-31 15:29:48 +0900

    feat: #10 - Textarea 컴포넌트 구현

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Providers } from './Providers/providers'
 export { default as ThemeButton } from './ThemeButton/themeButton'
 export { default as Input } from './common/Input/Input'
+export { default as Textarea } from './common/Textarea/Textarea'
