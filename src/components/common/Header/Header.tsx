25c4a07ffa9580b8848e5a7fc5aeda86a7b4e302
권기범 <bomi@gwongibeom-ui-MacBookAir.local>
2023-11-01 02:02:20 +0900

    feat: #30 - 헤더 타이틀 동적으로 변경

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,11 +1,19 @@
+'use client'
+
 import { LinkIcon } from '@heroicons/react/20/solid'
 import { BellIcon } from '@heroicons/react/24/outline'
 import { MagnifyingGlassCircleIcon } from '@heroicons/react/24/outline'
 import { Bars3Icon } from '@heroicons/react/24/solid'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import Button from '../Button/Button'
 
 const Header = () => {
+  const pathName = usePathname()
+  const currentPage = pathName
+    .split('/')[1]
+    .replace(/^[a-z]/, (char) => char.toUpperCase())
+
   return (
     <div className="flex items-center justify-between bg-bgColor">
       <div className="flex items-center justify-center">
@@ -16,7 +24,7 @@ const Header = () => {
         </Button>
       </div>
       <div className="absolute left-1/2 flex -translate-x-1/2 items-center justify-center">
-        헤더타이틀
+        {currentPage}
       </div>
       <div className="flex items-center justify-center">
         <Button className="flex h-8 w-8 items-center justify-center">
