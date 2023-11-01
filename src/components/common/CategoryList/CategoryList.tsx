eefbd3b15ec80cd43f6a9c135c0b6ddff57132d3
eeseung <seunghee9941@gmail.com>
2023-11-01 13:44:39 +0900

    feat: #26 - CategoryList 컴포넌트 구현

---
+++ src/components/common/CategoryList/CategoryList.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { useState } from 'react'
+import { cls } from '@/utils'
+import CategoryListItem from './CategoryListItem'
+import { CATEGORIES } from './constants'
+
+export interface CategoryListProps {
+  type?: 'all' | 'all_follow' | 'default'
+  horizontal?: boolean
+  onChange: (e?: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+const CategoryList = ({
+  type = 'default',
+  horizontal = true,
+  onChange,
+}: CategoryListProps) => {
+  const [index, setIndex] = useState(0)
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>, i: number) => {
+    setIndex(i)
+    onChange?.(e)
+  }
+
+  return (
+    <ul
+      className={cls(
+        'flex w-full gap-1.5 py-4',
+        horizontal
+          ? 'snap-x scroll-px-4 overflow-x-auto  scroll-smooth'
+          : 'flex-wrap px-4',
+      )}>
+      {CATEGORIES[type].map((category, i) => (
+        <li
+          className={cls(
+            'shrink-0',
+            horizontal && 'snap-end first:pl-4 last:pr-4',
+          )}
+          key={category}>
+          <CategoryListItem
+            label={category}
+            active={index === i}
+            onClick={(e) => handleClick(e, i)}
+          />
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default CategoryList
