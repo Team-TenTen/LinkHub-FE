22c23da26b201bdca1e7f69df7b49b4f8684749a
eeseung <seunghee9941@gmail.com>
2023-11-01 16:52:28 +0900

    style: #26 - CategoryListItem border 수정

--- src/components/common/CategoryList/CategoryListItem.tsx
+++ src/components/common/CategoryList/CategoryListItem.tsx
@@ -18,7 +18,9 @@ const CategoryListItem = ({
       value={label}
       className={cls(
         'rounded-3xl border px-4 py-2 text-sm font-medium',
-        active ? 'border-slate7 bg-slate5 text-white' : 'text-slate6',
+        active
+          ? 'border-slate-700 bg-slate5 text-white dark:border-slate-400'
+          : 'border-slate3 text-slate6',
       )}
       disabled={disabled}
       onClick={onClick}>
