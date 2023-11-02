429e1e061e2839773b1d444822675386e068b366
eeseung <seunghee9941@gmail.com>
2023-11-02 13:56:29 +0900

    feat: #22 - Dropdown 컴포넌트 구현

---
+++ src/components/common/Dropdown/constants/index.ts
@@ -0,0 +1,17 @@
+export const TYPES = {
+  space: ['최신순', '즐겨찾기순'],
+  link: ['최신순', '좋아요순'],
+  search: ['스페이스', '유저'],
+  user: ['Can edit', 'Can view', 'Remove'],
+}
+
+export const VERTICAL_PADDING = {
+  large: 'py-2.5',
+  medium: 'py-1.5',
+  small: 'py-0.5',
+}
+
+export const PLACEMENTS = {
+  left: 'left-0',
+  right: 'right-0',
+}
---
+++ src/components/common/Dropdown/hooks/useDropdown.ts
@@ -0,0 +1,53 @@
+'use client'
+
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react'
+
+export interface useDropdownProps {
+  el: React.RefObject<HTMLDivElement>
+  onChange: (e?: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+const useDropdown = ({
+  el,
+  onChange,
+}: useDropdownProps): {
+  isOpen: boolean
+  setIsOpen: Dispatch<SetStateAction<boolean>>
+  index: number
+  handleClick: (e: React.MouseEvent<HTMLButtonElement>, i: number) => void
+} => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    const handleOutsideClose = (e: { target: any }) => {
+      if (el.current !== null && !el.current?.contains(e.target)) {
+        setIsOpen(false)
+      }
+    }
+    document.addEventListener('click', handleOutsideClose)
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClose)
+    }
+  }, [isOpen, el])
+
+  const handleClick = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>, i: number) => {
+      setIndex(i)
+      onChange?.(e)
+      setIsOpen(false)
+    },
+    [onChange, setIsOpen],
+  )
+
+  return { isOpen, setIsOpen, index, handleClick }
+}
+
+export default useDropdown
