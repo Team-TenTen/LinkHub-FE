a91d8409d264fe337660f6ebf24f8fabc72da1af
권기범 <bomi@gwongibeom-ui-MacBookAir.local>
2023-11-04 03:56:04 +0900

    feat: #31 - 스페이스 멤버 추가 버튼 적용

--- src/components/common/SpaceMemberList/SpaceMemberList.tsx
+++ src/components/common/SpaceMemberList/SpaceMemberList.tsx
@@ -1,6 +1,10 @@
 'use client'
 
+import { Input } from '@/components'
+import { useModal } from '@/hooks'
+import { PlusSmallIcon } from '@heroicons/react/24/solid'
 import Avatar from '../Avatar/Avatar'
+import Button from '../Button/Button'
 import Dropdown from '../Dropdown/Dropdown'
 import DropdownItem from '../Dropdown/DropdownItem'
 import { SPACE_MEMBER } from './constants'
@@ -18,9 +22,51 @@ export interface SpaceMemberListProps {
 }
 
 const SpaceMemberList = ({ members, edit = false }: SpaceMemberListProps) => {
+  const { Modal, isOpen, modalOpen, modalClose } = useModal(false)
+
+  const handleConfirm = () => {
+    // 멤버 추가 로직
+    console.log('멤버가 추가 되었습니다.')
+  }
+
   return (
     <div className="flex flex-col">
-      <div className="py-4 text-base font-bold text-gray9">{SPACE_MEMBER}</div>
+      <div className="flex items-center justify-between">
+        <div className="py-4 text-base font-bold text-gray9">
+          {SPACE_MEMBER}
+        </div>
+        {edit && (
+          <Button
+            className="button button-sm button-emerald"
+            onClick={() => modalOpen()}>
+            <PlusSmallIcon className="h-5 w-5" />
+          </Button>
+        )}
+        {isOpen && (
+          <Modal
+            title="스페이스 멤버 추가"
+            isConfirmButton={true}
+            confirmText="추가"
+            onClose={modalClose}
+            onConfirm={handleConfirm}>
+            <div className="flex items-end justify-between gap-3 pb-6">
+              <div className="w-full">
+                <Input
+                  label="이메일"
+                  placeholder="추가할 멤버"
+                />
+              </div>
+              <div className="shrink-0">
+                <Dropdown
+                  size="large"
+                  type="user_invite"
+                  onChange={(e) => console.log(e?.currentTarget.value)}
+                />
+              </div>
+            </div>
+          </Modal>
+        )}
+      </div>
       {members.map((member) => (
         <div
           key={member.id}
