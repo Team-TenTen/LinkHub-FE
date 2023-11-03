6db12339923a59112b4d2a9de283bcc1edae0385
Yeongjun Kim <kyjun519@gmail.com>
2023-11-04 02:48:48 +0900

    feat: #43 - 링크 추가 버튼에 모달 연결

--- src/components/common/LinkList/LinkList.tsx
+++ src/components/common/LinkList/LinkList.tsx
@@ -1,4 +1,8 @@
+'use client'
+
+import { useModal } from '@/hooks'
 import { cls } from '@/utils'
+import Input from '../Input/Input'
 import LinkItem from '../LinkItem/LinkItem'
 import { ADD_LINK } from './constants'
 
@@ -25,36 +29,58 @@ const LinkList = ({
   edit = false,
   type = 'list',
 }: LinkListProps) => {
+  const { Modal, isOpen, modalOpen, modalClose } = useModal()
+
   return (
-    <div
-      className={cls(
-        type === 'list'
-          ? 'flex flex-col border border-slate3 last:border-b'
-          : 'grid grid-cols-2 gap-2',
-      )}>
-      <button
+    <>
+      {isOpen && (
+        <Modal
+          title="링크 추가"
+          isConfirmButton={true}
+          confirmText="추가"
+          onClose={modalClose}>
+          <div className="flex flex-col gap-2">
+            <Input
+              label="URl"
+              inputButton={true}></Input>
+            <Input
+              label="이름"
+              disabled={true}></Input>
+            <Input label="태그"></Input>
+          </div>
+        </Modal>
+      )}
+      <div
         className={cls(
-          'flex bg-slate-100 px-3 py-2.5 text-sm font-medium text-gray9 dark:bg-slate-800',
           type === 'list'
-            ? 'border-t border-slate3'
-            : 'items-center justify-center rounded-md border',
+            ? 'flex flex-col border border-slate3 last:border-b'
+            : 'grid grid-cols-2 gap-2',
         )}>
-        <div className="text-gray9">{ADD_LINK}</div>
-      </button>
-      {links.map((link) => (
-        <LinkItem
-          key={link.id}
-          title={link.title}
-          tag={link.tag}
-          readUsers={link.readUsers}
-          likes={link.likes}
-          read={read}
-          summary={summary}
-          edit={edit}
-          type={type}
-        />
-      ))}
-    </div>
+        <button
+          className={cls(
+            'flex bg-slate-100 px-3 py-2.5 text-sm font-medium text-gray9 dark:bg-slate-800',
+            type === 'list'
+              ? 'border-t border-slate3'
+              : 'items-center justify-center rounded-md border',
+          )}
+          onClick={modalOpen}>
+          <div className="text-gray9">{ADD_LINK}</div>
+        </button>
+        {links.map((link) => (
+          <LinkItem
+            key={link.id}
+            title={link.title}
+            tag={link.tag}
+            readUsers={link.readUsers}
+            likes={link.likes}
+            read={read}
+            summary={summary}
+            edit={edit}
+            type={type}
+          />
+        ))}
+      </div>
+    </>
   )
 }
 
