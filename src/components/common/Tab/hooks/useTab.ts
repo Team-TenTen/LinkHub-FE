ff173903098955ea2756ca42a68b0c516c4c44bb
Yeongjun Kim <kyjun519@gmail.com>
2023-11-05 00:21:16 +0900

    feat: #43 - useTab 훅 구현

---
+++ src/components/common/Tab/hooks/useTab.ts
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import { Space } from '@/types'
+import { getCurrentTabList } from '../utils'
+
+export interface TabList {
+  text: string
+  content: string
+  dest: string
+}
+
+const useTab = (
+  spaceData: Space,
+): {
+  currentTab: number
+  handleChangeTab: (index: number) => void
+  tabList: TabList[]
+} => {
+  const [currentTab, setCurrentTab] = useState(0)
+  const handleChangeTab = (index: number) => setCurrentTab(index)
+  const tabList = getCurrentTabList(spaceData)
+  return { currentTab, handleChangeTab, tabList }
+}
+
+export default useTab
