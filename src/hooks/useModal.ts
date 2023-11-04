748f30aed1178292104feb747bb6503df997a117
Yeongjun Kim <kyjun519@gmail.com>
2023-11-05 01:10:26 +0900

    feat: #43 - useModal 훅 리턴 타입 지정

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,7 +1,14 @@
 import { useCallback, useState } from 'react'
 import { Modal } from '@/components'
 
-const useModal = (initialState = false) => {
+export interface UseModalReturnType {
+  Modal: typeof Modal
+  isOpen: boolean
+  modalOpen: VoidFunction
+  modalClose: VoidFunction
+}
+
+const useModal = (initialState = false): UseModalReturnType => {
   const [isOpen, setIsOpen] = useState(initialState)
   const modalOpen = useCallback(() => setIsOpen(true), [])
   const modalClose = useCallback(() => setIsOpen(false), [])
