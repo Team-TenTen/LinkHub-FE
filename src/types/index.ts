b070b91285e713d271dbb4765b9d89cc1969c97b
Yeongjun Kim <kyjun519@gmail.com>
2023-11-05 00:17:56 +0900

    chore: #43 - Space 타입 선언

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export interface Space {
+  userName: string
+  spaceId: number
+  spaceImage: string
+  spaceName: string
+  description: string
+  category: string
+  favorite: number
+  scrap: number
+  comment: boolean
+}
