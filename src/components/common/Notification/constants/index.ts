65887d40193c3eb4d3e354f2b0589e6dc422298c
Yeongjun Kim <kyjun519@gmail.com>
2023-11-06 04:34:47 +0900

    feat: #50 - 알림 컴포넌트에서 사용되는 텍스트를 상수로 저장

---
+++ src/components/common/Notification/constants/index.ts
@@ -0,0 +1,8 @@
+export const NOTIFICATION_MSG = {
+  APPROVE_BUTTON: '수락',
+  APPROVE: '수락 완료',
+  USER: ' 님이 ',
+  FOLLOW: ' 님이 팔로우했습니다.',
+  COMMENT: ' 스페이스에 댓글을 남겼습니다.',
+  SPACE: ' 스페이스에 초대했습니다.',
+}
