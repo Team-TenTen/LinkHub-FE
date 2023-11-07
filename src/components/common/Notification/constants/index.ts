5df506b8e02a5f95ce8c0d3308b3a3ce76fd64b9
Yeongjun Kim <kyjun519@gmail.com>
2023-11-07 17:22:02 +0900

    feat: #50 - 상수 수정

--- src/components/common/Notification/constants/index.ts
+++ src/components/common/Notification/constants/index.ts
@@ -2,9 +2,9 @@ export const NOTIFICATION_MSG = {
   APPROVE_BUTTON: '수락',
   APPROVE: '수락 완료',
   USER: ' 님이 ',
-  FOLLOW: ' 님이 팔로우했습니다.',
+  FOLLOW: '팔로우했습니다.',
   COMMENT: '댓글',
   COMMENT_LEAVE: '을 남겼습니다.',
   SPACE: ' 스페이스에 ',
-  SPACE_INVITE: ' 스페이스에 초대했습니다.',
+  SPACE_INVITE: '초대했습니다.',
 }
