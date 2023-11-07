5fd0a9c92eec796ea3875a86ce64ab9d54531240
Yeongjun Kim <kyjun519@gmail.com>
2023-11-08 04:09:46 +0900

    feat: #52 - 알림 페이지 구현

--- src/app/(routes)/notification/page.tsx
+++ src/app/(routes)/notification/page.tsx
@@ -1,5 +1,28 @@
+'use client'
+
+import { Notification } from '@/components'
+import { mock_notificationData } from '@/data'
+
 const NotificationPage = () => {
-  return <></>
+  const notifications = mock_notificationData
+
+  return (
+    <div className="flex flex-col gap-2">
+      {notifications.map((notification) => (
+        <Notification
+          key={notification.id}
+          notificationId={notification.id}
+          type={notification.type}
+          userId={notification.userId}
+          userName={notification.userName}
+          spaceId={notification.spaceId}
+          spaceName={notification.spaceName}
+          onAccept={() => console.log('스페이스 수락')}
+          onClose={() => console.log('알람 닫기')}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default NotificationPage
