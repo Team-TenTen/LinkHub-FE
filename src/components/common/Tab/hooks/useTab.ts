54606b749834b23e43aee16d68aa161e63232338
Yeongjun Kim <kyjun519@gmail.com>
2023-11-08 03:47:50 +0900

    feat: #52 - useTab 훅을 유저, 알림 페이지에서도 사용할 수 있도록 수정

--- src/components/common/Tab/hooks/useTab.ts
+++ src/components/common/Tab/hooks/useTab.ts
@@ -1,6 +1,17 @@
-import { Space } from '@/types'
+import { Space, UserData } from '@/types'
 import { usePathname } from 'next/navigation'
-import { getCurrentTabList } from '../utils'
+import { NOTIFICATION_TAB_LIST } from '../constants'
+import {
+  getCurrentSpaceTabList,
+  getCurrentUserTabList,
+  getPathname,
+} from '../utils'
+
+export interface UseTabProps {
+  type: 'space' | 'notification' | 'user'
+  spaceData?: Space
+  userData?: UserData
+}
 
 export interface TabList {
   text: string
@@ -8,16 +19,36 @@ export interface TabList {
   dest: string
 }
 
-const useTab = (
-  spaceData: Space,
-): {
+export interface useTabReturn {
   currentTab: string
   tabList: TabList[]
-} => {
-  const currentTab = usePathname().split('/')[3] ?? 'space'
-  const tabList = getCurrentTabList(spaceData)
+}
 
-  return { currentTab, tabList }
+const useTab = ({ type, spaceData, userData }: UseTabProps): useTabReturn => {
+  const pathname = usePathname()
+
+  if (type === 'space') {
+    const currentTab = getPathname({ path: pathname, n: 3, defaultPath: type })
+    const tabList = getCurrentSpaceTabList(spaceData!)
+    return { currentTab, tabList }
+  }
+
+  if (type === 'notification') {
+    const currentTab = getPathname({ path: pathname, n: 3, defaultPath: type })
+    const tabList = NOTIFICATION_TAB_LIST
+    return { currentTab, tabList }
+  }
+
+  if (type === 'user') {
+    const currentTab = getPathname({ path: pathname, n: 3, defaultPath: type })
+    const tabList = getCurrentUserTabList(userData!)
+    return { currentTab, tabList }
+  }
+
+  return {
+    currentTab: '',
+    tabList: [{ text: '', content: '', dest: '' }],
+  }
 }
 
 export default useTab
