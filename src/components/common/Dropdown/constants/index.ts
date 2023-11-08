46144d2d45c00583040005d898b2686506f625e9
eeseung <seunghee9941@gmail.com>
2023-11-09 04:23:57 +0900

    fix: #57 - 드롭다운 옵션 오타 수정

--- src/components/common/Dropdown/constants/index.ts
+++ src/components/common/Dropdown/constants/index.ts
@@ -2,7 +2,7 @@ export const DROPDOWN_OPTIONS = {
   space: { 최신순: 'recent', 즐겨찾기순: 'scrap' },
   link: { 최신순: 'recent', 좋아요순: 'favorite' },
   search: { 스페이스: 'space', 유저: 'user' },
-  user_edit: { '편집 혀용': 'edit', '읽기 허용': 'view', 제거: 'remove' },
+  user_edit: { '편집 허용': 'edit', '읽기 허용': 'view', 제거: 'remove' },
   user_invite: { '편집 허용': 'eidt', '읽기 허용': 'view' },
 } as const
 
