85adfad5fff1108f1d38ce7c3774cca5e2046d96
Yeongjun Kim <kyjun519@gmail.com>
2023-11-08 17:33:02 +0900

    feat: #61 - userData 변경

--- src/app/(routes)/user/[userId]/layout.tsx
+++ src/app/(routes)/user/[userId]/layout.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 import Tab from '@/components/common/Tab/Tab'
 import TabItem from '@/components/common/Tab/TabItem'
 import useTab from '@/components/common/Tab/hooks/useTab'
-import { mock_userData } from '@/data'
+import { mock_userData2 } from '@/data'
 
 const UserLayout = ({ children }: { children: React.ReactNode }) => {
-  const userData = mock_userData
+  const userData = mock_userData2
   const { currentTab, tabList } = useTab({ type: 'user', userData })
 
   const childrenComponent = React.Children.only(children) as React.ReactElement
