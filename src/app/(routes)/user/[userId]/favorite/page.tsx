224b5f419b4324ef8622325ce72d2dd13de96368
Yeongjun Kim <kyjun519@gmail.com>
2023-11-08 17:52:36 +0900

    feat: #61 - 유저 즐겨찾기 페이지 구현

--- src/app/(routes)/user/[userId]/favorite/page.tsx
+++ src/app/(routes)/user/[userId]/favorite/page.tsx
@@ -1,5 +1,41 @@
+'use client'
+
+import { CategoryList, Input } from '@/components'
+import Space from '@/components/common/Space/Space'
+import { mock_userData2 } from '@/data'
+
 const UserFavoritePage = () => {
-  return <></>
+  const spaceData = mock_userData2.favoriteSpaces
+  return (
+    <>
+      <CategoryList
+        type="all"
+        horizontal={true}
+        onChange={(e) => console.log(e?.currentTarget.value)}
+      />
+      <Input
+        inputButton={true}
+        buttonText="검색"
+        buttonColor="gray"
+        onButtonClick={() => console.log('검색 로직 추가')}
+      />
+      <div className="flex flex-col gap-2 py-4">
+        {spaceData.map((space) => (
+          <Space
+            key={space.spaceId}
+            type="Card"
+            userName={space.userName}
+            spaceName={space.spaceName}
+            spaceImage={space.spaceImage}
+            description={space.description}
+            category={space.category}
+            scrap={space.scrap}
+            favorite={space.favorite}
+          />
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default UserFavoritePage
