5c089493424208f1cc293b44282e51e5570064e5
권기범 <bomi@gwongibeom-ui-MacBookAir.local>
2023-11-11 05:32:28 +0900

    feat: #47 - api 연결 예제(ing)

--- src/app/(routes)/bomiTest/apiTest/[spaceId]/page.tsx
+++ src/app/(routes)/bomiTest/apiTest/[spaceId]/page.tsx
@@ -1,5 +1,4 @@
-'use client'
-
+import Space from '@/components/common/Space/Space'
 import { getSpaceDetail } from '@/services/space/space'
 
 interface ApiTestProps {
@@ -8,15 +7,58 @@ interface ApiTestProps {
   }
 }
 
-const ApiTest = ({ params }: ApiTestProps) => {
-  const handleClickButton = async () => {
-    const data = await getSpaceDetail(params.spaceId)
-    console.log(data)
-  }
+interface MemberDetailInfo {
+  memberId: number
+  nickname: string
+  aboutMe: string
+  profilePath: string
+  SpaceMemberRole: string
+}
+interface SpaceProps {
+  spaceId: number
+  spaceName: string
+  description: string
+  category: string
+  isVisible: boolean
+  isComment: boolean
+  isLinkSummarizable: boolean
+  isReadMarkEnabled: boolean
+  viewCount: number
+  scrapCount: number
+  favoriteCount: number
+  spaceImagePath: string
+  memberDetailInfos: MemberDetailInfo[]
+  isOwner: boolean
+}
 
+const getSpaceData = async (id: string): Promise<SpaceProps> => {
+  const res = await getSpaceDetail(id)
+  return res
+}
+
+const ApiTest = async ({ params }: ApiTestProps) => {
+  const spaceData: SpaceProps = await getSpaceData(params.spaceId)
+
+  // next에서 이러한 패턴을 권장했는데 왜 안될까용...?? ㅠㅠ
+  const [space] = await Promise.all([spaceData])
+
+  // 콘솔에는 잘찍히는데
+  console.log(space)
+
+  // 렌더링이 되지 않는 모습
   return (
     <div>
-      <button onClick={handleClickButton}>버튼</button>
+      <Space
+        type="Card"
+        userName="tester"
+        spaceId={space.spaceId}
+        spaceName={space.spaceName}
+        spaceImage={space.spaceImagePath}
+        description={space.description}
+        category={space.category}
+        scrap={space.scrapCount}
+        favorite={space.favoriteCount}
+      />
     </div>
   )
 }
