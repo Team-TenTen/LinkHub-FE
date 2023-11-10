977c84c89dedd866508770e5a9f1c7883df188b7
Yeongjun Kim <kyjun519@gmail.com>
2023-11-10 17:29:47 +0900

    feat: #61 - getFollowChecked 함수 구현

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,22 @@
+import { PROFILE_MSG } from '@/constants'
+import { UserData } from '@/types'
+
 export const cls = (...classnames: (string | boolean | undefined)[]) => {
   return classnames.join(' ')
+}
+
+export interface GetFollowCheckedProps {
+  userData: UserData
+  myId: number
+}
+
+export const getFollowChecked = ({
+  userData,
+  myId,
+}: GetFollowCheckedProps): boolean => {
+  if (userData.follower.find((user) => user.userId === myId)) {
+    return true
+  } else {
+    return false
+  }
 }
