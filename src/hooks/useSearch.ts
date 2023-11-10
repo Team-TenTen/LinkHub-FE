abf7e220fa5f6a78f9375600dfc5e43cce9032cc
Yeongjun Kim <kyjun519@gmail.com>
2023-11-10 21:22:45 +0900

    feat: #61 - useSearch 훅 구현

---
+++ src/hooks/useSearch.ts
@@ -0,0 +1,58 @@
+import { useCallback, useMemo } from 'react'
+import { UseFormSetValue } from 'react-hook-form'
+import { SearchFormValue } from '@/app/(routes)/user/[userId]/space/page'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+
+export interface UseSearchProps {
+  setValue: UseFormSetValue<SearchFormValue>
+}
+
+const useSearch = ({ setValue }: UseSearchProps) => {
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const params = useMemo(
+    () => new URLSearchParams(searchParams),
+    [searchParams],
+  )
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      params.set(name, value)
+      return params.toString()
+    },
+    [params],
+  )
+
+  const handleCategoryChange = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      router.replace(
+        pathname + '?' + createQueryString('category', e.currentTarget.value),
+        { scroll: false },
+      )
+    },
+    [router, pathname, createQueryString],
+  )
+
+  const onSubmit = useCallback(
+    ({ keyword, path }: { keyword: string; path: string }) => {
+      console.log(params.get('category'))
+      setValue('keyword', '')
+      if (params.get('category')) {
+        router.push(
+          `${path}?category=${params.get('category')}&keyword=${keyword}`,
+        )
+      } else {
+        router.push(`${path}?category=전체&keyword=${keyword}`)
+      }
+    },
+    [router, params, , setValue],
+  )
+
+  return {
+    handleCategoryChange,
+    onSubmit,
+  }
+}
+
+export default useSearch
