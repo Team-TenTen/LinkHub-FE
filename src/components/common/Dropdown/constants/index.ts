07532fad5d8b953d62ff7f3c9c4a3704c00870a3
eeseung <seunghee9941@gmail.com>
2023-11-12 05:29:13 +0900

    feat: #63 - Dropdown 컴포넌트 상수 수정

--- src/components/common/Dropdown/constants/index.ts
+++ src/components/common/Dropdown/constants/index.ts
@@ -4,15 +4,15 @@ export const DROPDOWN_OPTIONS = {
   search: { 스페이스: 'space', 유저: 'user' },
   user_edit: { '편집 허용': 'edit', '읽기 허용': 'view', 제거: 'remove' },
   user_invite: { '편집 허용': 'eidt', '읽기 허용': 'view' },
-} as const
+}
 
 export const VERTICAL_PADDING = {
   large: 'py-2.5',
   medium: 'py-1.5',
   small: 'py-0.5',
-} as const
+}
 
 export const PLACEMENTS = {
   left: 'left-0',
   right: 'right-0',
-} as const
+}
