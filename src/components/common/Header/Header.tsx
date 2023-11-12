20e90aedd93d50b603b08533467686416a31c614
eeseung <seunghee9941@gmail.com>
2023-11-12 16:41:39 +0900

    feat: #63 - Header 컴포넌트 검색 모달 searchParams 수정

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -21,6 +21,26 @@ const Header = () => {
     .split(/[^a-zA-Z]/)[1] // 라우터명
     .replace(/^[a-z]/, (char) => char.toUpperCase()) // 첫글자 대문자 치환
 
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams)
+      params.set(name, value)
+
+      return params.toString()
+    },
+    [searchParams],
+  )
+
+  const deleteQueryString = useCallback(
+    (name: string) => {
+      const params = new URLSearchParams(searchParams)
+      params.delete(name)
+
+      return params.toString()
+    },
+    [searchParams],
+  )
+
   return (
     <>
       <div className="fixed z-50 flex w-full max-w-[500px] items-center justify-between border-b border-slate-100 bg-bgColor px-4 py-2.5 dark:border-slate-800">
@@ -42,7 +62,12 @@ const Header = () => {
           </Button>
           <Button
             className="flex h-8 w-8 items-center justify-center"
-            onClick={() => router.push(`${pathname}?search=true`)}>
+            onClick={() =>
+              router.replace(
+                pathname + '?' + createQueryString('search', 'true'),
+                { scroll: false },
+              )
+            }>
             <MagnifyingGlassCircleIcon className="h-6 w-6 text-slate9" />
           </Button>
           <Button
@@ -53,7 +78,15 @@ const Header = () => {
         </div>
       </div>
       {isSidebarOpen && <Sidebar onClose={() => setIsSidebarOpen(false)} />}
-      {isSearchModalOpen && <SearchModal onClose={() => router.back()} />}
+      {isSearchModalOpen && (
+        <SearchModal
+          onClose={() =>
+            router.replace(pathname + '?' + deleteQueryString('search'), {
+              scroll: false,
+            })
+          }
+        />
+      )}
     </>
   )
 }
