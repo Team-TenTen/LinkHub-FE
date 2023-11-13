5c4abac3d565daa03ba2ec2fd22adf40faa9a21e
권기범 <bomi@gwongibeom-ui-MacBookAir.local>
2023-11-13 13:28:54 +0900

    feat: #47 - 스페이스 상세 api 연결 예제

--- src/app/(routes)/bomiTest/apiTest/[spaceId]/page.tsx
+++ src/app/(routes)/bomiTest/apiTest/[spaceId]/page.tsx
@@ -7,45 +7,9 @@ interface ApiTestProps {
   }
 }
 
-interface MemberDetailInfo {
-  memberId: number
-  nickname: string
-  aboutMe: string
-  profilePath: string
-  SpaceMemberRole: string
-}
-interface SpaceProps {
-  spaceId: number
-  spaceName: string
-  description: string
-  category: string
-  isVisible: boolean
-  isComment: boolean
-  isLinkSummarizable: boolean
-  isReadMarkEnabled: boolean
-  viewCount: number
-  scrapCount: number
-  favoriteCount: number
-  spaceImagePath: string
-  memberDetailInfos: MemberDetailInfo[]
-  isOwner: boolean
-}
-
-const getSpaceData = async (id: string): Promise<SpaceProps> => {
-  const res = await getSpaceDetail(id)
-  return res
-}
-
 const ApiTest = async ({ params }: ApiTestProps) => {
-  const spaceData: SpaceProps = await getSpaceData(params.spaceId)
+  const { space } = await getSpaceDetail(params.spaceId)
 
-  // next에서 이러한 패턴을 권장했는데 왜 안될까용...?? ㅠㅠ
-  const [space] = await Promise.all([spaceData])
-
-  // 콘솔에는 잘찍히는데
-  console.log(space)
-
-  // 렌더링이 되지 않는 모습
   return (
     <div>
       <Space
