918fc3cb666a53619f3331a5c7363e801516f228
eeseung <seunghee9941@gmail.com>
2023-11-15 04:13:06 +0900

    chore: #78 - appspec.yml & scripts 수정

---
+++ scripts/after-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+REPOSITORY=/home/ubuntu/deploy
+
+cd $REPOSITORY
+
+nvm use
+npm install
