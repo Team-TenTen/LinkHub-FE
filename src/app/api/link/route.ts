613642fe0a0adb68b785189669e016bfa4b13893
Yeongjun Kim <kyjun519@gmail.com>
2023-11-16 05:15:21 +0900

    feat: #97 - 링크 생성 api 함수 추가

---
+++ src/app/api/link/route.ts
@@ -0,0 +1,23 @@
+import { apiServer } from '@/services/apiServices'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const { spaceId, url, title, tag, color } = await req.json()
+  const path = `/spaces/${spaceId}/links`
+  const body = { url, title, tag, color }
+  const token = req.cookies.get('token')
+
+  const authorization = {
+    Authorization: `Bearer ${token?.value}`,
+  }
+  try {
+    const data = await apiServer.post(path, body, {}, authorization)
+
+    return NextResponse.json({ data })
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.response.data.message },
+      { status: error.response.status },
+    )
+  }
+}
