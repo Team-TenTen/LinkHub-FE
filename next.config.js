5db094c53827e6885932933718f8a8e1673de857
권기범 <bomi@gwongibeom-ui-MacBookAir.local>
2023-11-17 01:38:31 +0900

    feat: #74 - 로그인 로직

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/kakaoLogin',
+        destination: `${process.env.NEXT_PUBLIC_API_ADDRESS}/oauth2/authorization/kakao`,
+        permanent: true,
+      },
+      {
+        source: '/oauth2/redirection/kakao(.*)',
+        destination: '/login',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
