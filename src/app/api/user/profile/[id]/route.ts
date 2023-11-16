df347ac7123c08d431ff9fe8a46e04ca87b5385a
Yeongjun Kim <kyjun519@gmail.com>
2023-11-16 18:14:50 +0900

    feat: #102 - 유저 프로필을 조회하는 api 함수 구현

---
+++ src/app/api/user/profile/[id]/route.ts
@@ -0,0 +1,17 @@
+import { apiServer } from '@/services/apiServices'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  const userId = req.nextUrl.pathname.replace('/api/user/profile/', '')
+  const path = `/members/${userId}/profile`
+
+  try {
+    const user = await apiServer.get(path)
+    return NextResponse.json(user)
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.response.data.message },
+      { status: error.response.status },
+    )
+  }
+}
