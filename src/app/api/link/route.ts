de44a84169aed66f70a0cb54e5e0592cf941fa37
Yeongjun Kim <kyjun519@gmail.com>
2023-11-16 14:13:43 +0900

    chore: #97 - 공백 수정

--- src/app/api/link/route.ts
+++ src/app/api/link/route.ts
@@ -10,9 +10,9 @@ export async function POST(req: NextRequest, res: NextResponse) {
   const authorization = {
     Authorization: `Bearer ${token?.value}`,
   }
+
   try {
     const data = await apiServer.post(path, body, {}, authorization)
-
     return NextResponse.json({ data })
   } catch (error: any) {
     return NextResponse.json(
