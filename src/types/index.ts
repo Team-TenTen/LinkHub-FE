1c10dd3fcbc1e28bbbbe691e47102b693a7bd298
eeseung <seunghee9941@gmail.com>
2023-11-18 04:18:58 +0900

    feat: #104 - 메인 스페이스 조회 api response 타입 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -138,10 +138,21 @@ export interface SpaceReqBody {
 }
 
 // 링크 생성 req body
-
 export interface CreateLinkReqBody {
   url: string
   title: string
   tag: string
   color: string
+}
+
+export interface SpaceResBody {
+  spaceId: number
+  spaceName: string
+  description: string
+  userName: string
+  category: string
+  viewCount: number
+  scrapCount: number
+  favoriteCount: number
+  spaceImagePath: string
 }
