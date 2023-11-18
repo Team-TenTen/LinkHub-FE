19b4a8ce94ddbfc11307f87fa0a1b8e0444d6143
Yeongjun Kim <kyjun519@gmail.com>
2023-11-18 18:17:03 +0900

    feat: #103 - 카테고리를 한글로 번역하는 상수 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -20,3 +20,15 @@ export const COLOR_LIST = [
   'pink',
   'gray',
 ]
+
+export interface CategoriesRenderType {
+  [key: string]: string
+}
+
+export const CATEGORIES_RENDER: CategoriesRenderType = {
+  ENTER_ART: '엔터테인먼트•예술',
+  LIFE_KNOWHOW_SHOPPING: '생활•노하우•쇼핑',
+  HOBBY_LEISURE_TRAVEL: '취미•여가•여행',
+  KNOWLEDGE_ISSUE_CAREER: '지식•이슈•커리어',
+  ETC: '기타',
+}
