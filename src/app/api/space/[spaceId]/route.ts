608e80be1ece442cda48493cb149980175ad2bc5
Yeongjun Kim <kyjun519@gmail.com>
2023-11-19 17:35:06 +0900

    feat: #109 - 스페이스 상세 조회 api에 headers 전달 및 cache 옵션 지정

--- src/app/api/space/[spaceId]/route.ts
+++ src/app/api/space/[spaceId]/route.ts
@@ -4,9 +4,12 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function GET(req: NextRequest) {
   const spaceId = req.nextUrl.pathname.replace('/api/space/', '')
   const path = `/spaces/${spaceId}`
+  const headers = {
+    Authorization: `Bearer ${req.cookies.get('token')?.value}`,
+  }
 
   try {
-    const space = await apiServer.get(path)
+    const space = await apiServer.get(path, { cache: 'no-cache' }, headers)
     return NextResponse.json({ space })
   } catch (error: any) {
     return NextResponse.json(
