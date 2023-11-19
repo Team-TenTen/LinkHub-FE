13f9c5c64beb9155b286ae904268226873f2f47a
Yeongjun Kim <kyjun519@gmail.com>
2023-11-19 17:29:06 +0900

    feat: #109 - 즐겨찾기 추가 api 함수 구현

---
+++ src/app/api/favorites/[spaceId]/route.ts
@@ -0,0 +1,20 @@
+import { apiServer } from '@/services/apiServices'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function POST(req: NextRequest) {
+  const { spaceId } = await req.json()
+  const path = `/spaces/${spaceId}/favorites`
+  const headers = {
+    Authorization: `Bearer ${req.cookies.get('token')?.value}`,
+  }
+
+  try {
+    const data = await apiServer.post(path, {}, {}, headers)
+    return NextResponse.json({ data })
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.response.data.message },
+      { status: error.response.status },
+    )
+  }
+}
