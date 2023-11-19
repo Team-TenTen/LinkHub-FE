4c598e34097aec308a5c8fa652522cecca5eb9cf
Yeongjun Kim <kyjun519@gmail.com>
2023-11-19 17:29:30 +0900

    feat: #109 - 즐겨찾기 취소 api 함수 구현

--- src/app/api/favorites/[spaceId]/route.ts
+++ src/app/api/favorites/[spaceId]/route.ts
@@ -18,3 +18,21 @@ export async function POST(req: NextRequest) {
     )
   }
 }
+
+export async function DELETE(req: NextRequest) {
+  const spaceId = req.nextUrl.pathname.replace('/api/favorites/', '')
+  const path = `/spaces/${spaceId}/favorites`
+  const headers = {
+    Authorization: `Bearer ${req.cookies.get('token')?.value}`,
+  }
+
+  try {
+    const data = await apiServer.delete(path, {}, headers)
+    return NextResponse.json({ data })
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.response.data.message },
+      { status: error.response.status },
+    )
+  }
+}
