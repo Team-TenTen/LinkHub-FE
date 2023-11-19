f5cf15bf96ca29e8366e63d1594fd9975cb96135
Yeongjun Kim <kyjun519@gmail.com>
2023-11-19 23:58:49 +0900

    feat: #102 - useGetProfile 훅 구현

---
+++ src/hooks/useGetProfile.ts
@@ -0,0 +1,26 @@
+import { useCallback, useEffect, useState } from 'react'
+import { fetchGetUserProfile } from '@/services/user/profile/userProfile'
+import { UserProfileResBody } from '@/types'
+import { usePathname, useRouter } from 'next/navigation'
+import { useCurrentUser } from './useCurrentUser'
+
+const useGetProfile = () => {
+  const [user, setUser] = useState<UserProfileResBody>()
+  const path = usePathname()
+  const userId = Number(path.split('/')[2])
+  const { currentUser } = useCurrentUser()
+  const myId = currentUser?.memberId
+
+  const handleGetUserProfile = useCallback(async () => {
+    const userData = await fetchGetUserProfile({ userId })
+    setUser(userData)
+  }, [userId])
+
+  useEffect(() => {
+    handleGetUserProfile()
+  }, [handleGetUserProfile])
+
+  return { user, myId }
+}
+
+export default useGetProfile
