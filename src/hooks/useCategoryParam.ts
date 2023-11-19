da5fa7e848c285a12a78322f052f6302dc3b107f
eeseung <seunghee9941@gmail.com>
2023-11-20 04:54:32 +0900

    feat: #77 - useCategoryParam 훅 구현

---
+++ src/hooks/useCategoryParam.ts
@@ -0,0 +1,28 @@
+import { CATEGORIES } from '@/components/common/CategoryList/constants'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import useCreateQueryString from './useCreateQueryString'
+
+const useCategoryParam = (type: keyof typeof CATEGORIES) => {
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const category = searchParams.get('category')
+  const categoryIndex = category
+    ? Object.values(CATEGORIES[type]).indexOf(category)
+    : 0
+  const { createQueryString } = useCreateQueryString(searchParams)
+
+  const handleCategoryChange = (e: React.MouseEvent<HTMLButtonElement>) => {
+    router.replace(
+      pathname + '?' + createQueryString('category', e.currentTarget.value),
+      { scroll: false },
+    )
+  }
+  return {
+    category,
+    categoryIndex,
+    handleCategoryChange,
+  }
+}
+
+export default useCategoryParam
