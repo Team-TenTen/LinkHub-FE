3f8915005ffa62321965e625410e8477a7573b7d
Yeongjun Kim <kyjun519@gmail.com>
2023-11-19 17:26:50 +0900

    feat: #109 - 즐겨찾기 추가 fetch 함수 구현

---
+++ src/services/favorites/favorites.ts
@@ -0,0 +1,19 @@
+import { apiClient } from '../apiServices'
+
+export interface FetchFavoriteSpaceProps {
+  spaceId: number
+}
+
+const fetchFavoriteSpace = async ({ spaceId }: FetchFavoriteSpaceProps) => {
+  const path = `/api/favorites/${spaceId}`
+  const body = { spaceId }
+
+  try {
+    const response = await apiClient.post(path, body)
+    return response.json()
+  } catch (e) {
+    if (e instanceof Error) throw new Error(e.message)
+  }
+}
+
+export { fetchFavoriteSpace }
