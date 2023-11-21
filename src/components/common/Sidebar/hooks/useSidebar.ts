0ac4847d8fd06d5bfdebef8fa98c59636aca7e23
권기범 <bomi@gwongibeom-ui-MacBookAir.local>
2023-11-22 02:45:44 +0900

    feat: #122 - 로그아웃 후 새로고침

--- src/components/common/Sidebar/hooks/useSidebar.ts
+++ src/components/common/Sidebar/hooks/useSidebar.ts
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { kakaoLogout } from '@/services/auth'
+import Cookies from 'js-cookie'
 
 type SpaceType = '내 스페이스' | '즐겨찾기'
 
@@ -14,10 +15,12 @@ const useSidebar = ({ sidebarRef, onClose }: useSidebarProps) => {
   const logout = async () => {
     try {
       await kakaoLogout()
+      Cookies.remove('Auth-token')
+      location.reload()
       alert('로그아웃 되었습니다.')
       onClose()
     } catch (e) {
-      alert('로그아웃에 실패하였습니다.')
+      alert('다시 시도해 주세요.')
     }
   }
 
