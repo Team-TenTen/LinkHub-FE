942112dc54fb688727eecec2948b405dbb9e3f73
Yeongjun Kim <kyjun519@gmail.com>
2023-11-22 18:18:52 +0900

    feat: #140 - 팔로워 목록 조회 api 함수 구현

---
+++ src/app/api/user/[memberId]/followers/route.ts
@@ -0,0 +1,26 @@
+import { useServerCookie } from '@/hooks/useServerCookie'
+import { apiServer } from '@/services/apiServices'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { memberId: number } },
+) {
+  const { token } = useServerCookie()
+  const { searchParams } = new URL(req.url)
+  const memberId = params.memberId
+  const path = `/members/${memberId}/followers`
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  }
+
+  try {
+    const data = await apiServer.get(`${path}?${searchParams}`, {}, headers)
+    return NextResponse.json(data)
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.response.data.errorMessage },
+      { status: error.response.status },
+    )
+  }
+}
