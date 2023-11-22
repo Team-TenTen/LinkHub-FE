5f5d8dfb4196dff121dfeb1b39264e79cebb340c
Yeongjun Kim <kyjun519@gmail.com>
2023-11-22 21:11:42 +0900

    feat: #140 - User 컴포넌트의 타입과 props 이름을 서버 데이터와 동일하게 수정

--- src/components/common/User/User.tsx
+++ src/components/common/User/User.tsx
@@ -4,52 +4,48 @@ import { cls } from '@/utils'
 import Link from 'next/link'
 import Avatar from '../Avatar/Avatar'
 import Button from '../Button/Button'
-import useToggle from '../Toggle/hooks/useToggle'
 
 interface UserProps {
-  id: number
-  name: string
-  profile: string
-  oneLiner?: string
-  isFollow?: boolean
+  memberId?: number
+  nickname: string
+  profileImagePath: string
+  aboutMe?: string
+  isFollowing?: boolean
   isAuth?: boolean
-  onClick?: (id: number, isFollow: boolean) => void
+  onClick?: (isFollowing: boolean) => void
 }
 
 const User = ({
-  id,
-  name,
-  profile,
-  oneLiner,
-  isFollow,
+  memberId,
+  nickname,
+  profileImagePath,
+  aboutMe,
+  isFollowing,
   isAuth,
   onClick,
 }: UserProps) => {
-  const [isFollowing, toggle] = useToggle(isFollow)
-
-  const handleButtonClick = () => {
-    toggle()
-    onClick?.(id, isFollowing)
+  const handleFollowButtonClick = () => {
+    onClick?.(!!isFollowing)
   }
 
   return (
     <div className="flex items-center gap-x-3 rounded-md border border-slate3 p-3">
       <div className="inline-flex shrink-0">
         <Avatar
-          src={profile}
+          src={profileImagePath}
           width={40}
           height={40}
-          alt={name}
+          alt="profile"
         />
       </div>
       <div className="flex grow flex-col gap-y-0.5 overflow-hidden py-0.5">
         <Link
-          href={`/user/${id}`}
+          href={`/user/${memberId}`}
           className="block overflow-hidden text-ellipsis whitespace-nowrap text-sm font-bold text-gray9">
-          {name}
+          {nickname}
         </Link>
         <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs text-gray6">
-          {oneLiner}
+          {aboutMe}
         </p>
       </div>
       {!isAuth && (
@@ -60,7 +56,7 @@ const User = ({
               'button px-2.5 py-1.5 text-sm',
               isFollowing ? 'button-white' : 'button-emerald',
             )}
-            onClick={handleButtonClick}>
+            onClick={handleFollowButtonClick}>
             {isFollowing ? '팔로잉' : '팔로우'}
           </Button>
         </div>
