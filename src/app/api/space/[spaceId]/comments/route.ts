1c5267e93ed9ab7802c1e9dd6017c36702e10f45
eeseung <seunghee9941@gmail.com>
2023-11-22 17:08:40 +0900

    feat: #133 - 루트 댓글 조회 api route handler 타입 수정

--- src/app/api/space/[spaceId]/comments/route.ts
+++ src/app/api/space/[spaceId]/comments/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { spaceId: string } },
+  { params }: { params: { spaceId: number } },
 ) {
   const { token } = useServerCookie()
   const spaceId = params.spaceId
