b67a094dba4a56d4c25c21f34bbcfd20cd7c2ca4
eeseung <seunghee9941@gmail.com>
2023-11-22 17:11:16 +0900

    feat: #133 - 루트 댓글 생성 api route handler 추가

---
+++ src/app/api/space/[spaceId]/comments/create/route.ts
@@ -0,0 +1,26 @@
+import { useServerCookie } from '@/hooks/useServerCookie'
+import { apiServer } from '@/services/apiServices'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { spaceId: number } },
+) {
+  const { token } = useServerCookie()
+  const spaceId = params.spaceId
+  const path = `/spaces/${spaceId}/comments`
+  const body = await req.json()
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  }
+
+  try {
+    const data = await apiServer.post(path, body, {}, headers)
+    return NextResponse.json(data)
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.response.data.errorMessage },
+      { status: error.response.status },
+    )
+  }
+}
