1a185a14b42b5109b79e4d93b3d80767141fc8b6
eeseung <seunghee9941@gmail.com>
2023-11-24 02:09:28 +0900

    feat: #132 - useCommentList 훅 열린 답글 닫는 기능 추가

--- src/components/CommentList/hooks/useCommentList.ts
+++ src/components/CommentList/hooks/useCommentList.ts
@@ -13,10 +13,8 @@ const useCommentList = ({
     (depth1: number, depth2: number) => {
       const nextOpenedComments = openedComments.map(
         (group: boolean[], groupIdx: number) =>
-          group.map((_, commentIdx: number) =>
-            groupIdx === depth1 && commentIdx === depth2
-              ? true
-              : openedComments[groupIdx][commentIdx],
+          group.map((opened, commentIdx: number) =>
+            groupIdx === depth1 && commentIdx === depth2 ? !opened : opened,
           ),
       )
       setOpenedComments(nextOpenedComments)
