1ae91e12c429f590129781f8e9bf5c3a8ec1eb18
Yeongjun Kim <kyjun519@gmail.com>
2023-11-23 13:57:55 +0900

    feat: #140 - 비로그인 유저가 FollowList 컴포넌트 안에서 팔로우 클릭 시 나타나는 로그인 알림 모달 추가

--- src/components/common/User/User.tsx
+++ src/components/common/User/User.tsx
@@ -1,9 +1,13 @@
 'use client'
 
+import { Dispatch, SetStateAction } from 'react'
+import { PROFILE_MSG } from '@/constants'
+import { useFollowUser, useModal } from '@/hooks'
 import { cls } from '@/utils'
 import Link from 'next/link'
 import Avatar from '../Avatar/Avatar'
 import Button from '../Button/Button'
+import LoginModal from '../Modal/LoginModal'
 
 interface UserProps {
   memberId?: number
@@ -12,7 +16,9 @@ interface UserProps {
   aboutMe?: string
   isFollowing?: boolean
   isAuth?: boolean
-  onClick?: (isFollowing: boolean) => void
+  followingCount?: number
+  myId?: number
+  setFollowingCount?: Dispatch<SetStateAction<number | undefined>>
 }
 
 const User = ({
@@ -22,46 +28,73 @@ const User = ({
   aboutMe,
   isFollowing,
   isAuth,
-  onClick,
+  followingCount,
+  myId,
+  setFollowingCount,
 }: UserProps) => {
-  const handleFollowButtonClick = () => {
-    onClick?.(!!isFollowing)
-  }
+  const { Modal, isOpen, modalClose, currentModal, handleOpenCurrentModal } =
+    useModal()
+  const { isFollowing: isFollowingValue, handleClickListInFollow } =
+    useFollowUser({
+      memberId: memberId || 0,
+      isInitFollowing: !!isFollowing,
+      followerInitCount: followingCount || 0,
+    })
 
   return (
-    <div className="flex items-center gap-x-3 rounded-md border border-slate3 p-3">
-      <div className="inline-flex shrink-0">
-        <Avatar
-          src={profileImagePath}
-          width={40}
-          height={40}
-          alt="profile"
-        />
-      </div>
-      <div className="flex grow flex-col gap-y-0.5 overflow-hidden py-0.5">
-        <Link
-          href={`/user/${memberId}`}
-          className="block overflow-hidden text-ellipsis whitespace-nowrap text-sm font-bold text-gray9">
-          {nickname}
-        </Link>
-        <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs text-gray6">
-          {aboutMe}
-        </p>
-      </div>
-      {!isAuth && (
-        <div className="shrink-0">
-          <Button
-            type="button"
-            className={cls(
-              'button px-2.5 py-1.5 text-sm',
-              isFollowing ? 'button-white' : 'button-emerald',
-            )}
-            onClick={handleFollowButtonClick}>
-            {isFollowing ? '팔로잉' : '팔로우'}
-          </Button>
+    <>
+      <div className="flex items-center gap-x-3 rounded-md border border-slate3 p-3">
+        <div className="inline-flex shrink-0">
+          <Avatar
+            src={profileImagePath}
+            width={40}
+            height={40}
+            alt="profile"
+          />
         </div>
+        <div className="flex grow flex-col gap-y-0.5 overflow-hidden py-0.5">
+          <Link
+            href={`/user/${memberId}`}
+            className="block overflow-hidden text-ellipsis whitespace-nowrap text-sm font-bold text-gray9">
+            {nickname}
+          </Link>
+          <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs text-gray6">
+            {aboutMe}
+          </p>
+        </div>
+        {!isAuth && (
+          <div className="shrink-0">
+            <Button
+              type="button"
+              className={cls(
+                'button px-2.5 py-1.5 text-sm',
+                isFollowingValue ? 'button-white' : 'button-emerald',
+              )}
+              onClick={() => {
+                if (myId) {
+                  handleClickListInFollow(isFollowingValue)
+                  if (isFollowingValue) {
+                    setFollowingCount?.((prev) => prev! - 1)
+                  } else {
+                    setFollowingCount?.((prev) => prev! + 1)
+                  }
+                } else {
+                  handleOpenCurrentModal('login')
+                }
+              }}>
+              {isFollowingValue ? PROFILE_MSG.FOLLOWING : PROFILE_MSG.FOLLOW}
+            </Button>
+          </div>
+        )}
+      </div>
+      {currentModal === 'login' && (
+        <LoginModal
+          Modal={Modal}
+          isOpen={isOpen}
+          modalClose={modalClose}
+        />
       )}
-    </div>
+    </>
   )
 }
 
