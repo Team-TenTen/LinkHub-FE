cca2be753d1c7c942bd41e5d3f75a9daa35ed1e7
Yeongjun Kim <kyjun519@gmail.com>
2023-11-24 03:16:24 +0900

    feat: #114 - useProfileFavoritesSearch 훅에 type prop 추가 및 basePath 지정

--- src/hooks/useProfileFavoritesSearch.ts
+++ src/hooks/useProfileFavoritesSearch.ts
@@ -5,23 +5,25 @@ import { useRouter } from 'next/navigation'
 export interface UseSpaceSearchProps {
   userId: number
   category: string
+  type: 'space' | 'favorite'
   setValue: UseFormSetValue<SearchFormValue>
 }
 
 const useProfileFavoritesSearch = ({
   userId,
   category,
+  type,
   setValue,
 }: UseSpaceSearchProps) => {
   const router = useRouter()
+  const basePath =
+    type === 'space' ? `/user/${userId}/space?` : `/user/${userId}/favorite?`
 
   const onSubmit: SubmitHandler<SearchFormValue> = (data) => {
     if (category) {
-      router.push(
-        `/user/${userId}/favorite?&category=${category}&keyword=${data.keyword}`,
-      )
+      router.push(`${basePath}category=${category}&keyword=${data.keyword}`)
     } else {
-      router.push(`/user/${userId}/favorite?keyword=${data.keyword}`)
+      router.push(`${basePath}keyword=${data.keyword}`)
     }
     setValue('keyword', '')
   }
