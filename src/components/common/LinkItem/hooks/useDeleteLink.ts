a9c0a5199f4c7562d59bb4f1afc31053a17b00e7
Yeongjun Kim <kyjun519@gmail.com>
2023-11-25 02:27:53 +0900

    feat: #149 - 링크 삭제 시 링크 목록이 업데이트 되도록 적용

--- src/components/common/LinkItem/hooks/useDeleteLink.ts
+++ src/components/common/LinkItem/hooks/useDeleteLink.ts
@@ -1,11 +1,15 @@
 import { FetchDeleteLinbkProps, fetchDeleteLink } from '@/services/link/link'
+import { useQueryClient } from '@tanstack/react-query'
 
 const useDeleteLink = () => {
+  const queryClient = useQueryClient()
+
   const handleDeleteLink = async ({
     spaceId,
     linkId,
   }: FetchDeleteLinbkProps) => {
     await fetchDeleteLink({ spaceId, linkId })
+    await queryClient.invalidateQueries({ queryKey: ['links'] })
   }
 
   return { handleDeleteLink }
