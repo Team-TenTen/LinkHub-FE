793b45703263002ff0dcad3848a99d49137bd6c1
eeseung <seunghee9941@gmail.com>
2023-11-29 15:18:06 +0900

    feat: #171 - Dropdown 컴포넌트 user_invite 영문 옵션 수정

--- src/components/common/Dropdown/constants/index.ts
+++ src/components/common/Dropdown/constants/index.ts
@@ -3,7 +3,7 @@ export const DROPDOWN_OPTIONS = {
   link: { 최신순: 'recent', 좋아요순: 'like' },
   search: { 스페이스: 'space', 유저: 'user' },
   user_edit: { '편집 허용': 'edit', '읽기 허용': 'view', 제거: 'remove' },
-  user_invite: { '편집 허용': 'eidt', '읽기 허용': 'view' },
+  user_invite: { '편집 허용': 'CAN_EDIT', '읽기 허용': 'CAN_VIEW' },
 }
 
 export const VERTICAL_PADDING = {
