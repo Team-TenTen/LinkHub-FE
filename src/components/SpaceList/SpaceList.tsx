7f8f3e21de50b70bbe7e9b9b79e3abf10e34118a
Yeongjun Kim <kyjun519@gmail.com>
2023-11-30 17:53:16 +0900

    feat: #189 - 스페이스 리스트 로딩 추가

--- src/components/SpaceList/SpaceList.tsx
+++ src/components/SpaceList/SpaceList.tsx
@@ -5,6 +5,7 @@ import { CATEGORIES_RENDER } from '@/constants'
 import useInfiniteScroll from '@/hooks/useInfiniteScroll'
 import { SearchSpaceReqBody, SpaceResBody } from '@/types'
 import Space from '../common/Space/Space'
+import Spinner from '../common/Spinner/Spinner'
 import { NONE_SEARCH_RESULT } from './constants'
 import useSpacesQuery from './hooks/useSpacesQuery'
 
@@ -32,46 +33,51 @@ const SpaceList = ({
   keyword,
   fetchFn,
 }: SpaceListProps) => {
-  const { spaces, fetchNextPage, hasNextPage } = useSpacesQuery({
-    queryKey,
-    memberId,
-    sort,
-    category,
-    keyword,
-    fetchFn,
-  })
+  const { spaces, fetchNextPage, hasNextPage, isSpacesLoading } =
+    useSpacesQuery({
+      queryKey,
+      memberId,
+      sort,
+      category,
+      keyword,
+      fetchFn,
+    })
   const { target } = useInfiniteScroll({ hasNextPage, fetchNextPage })
 
-  return (
-    <ul className="flex flex-col gap-y-2 px-4 pt-2">
-      {spaces?.pages[0].responses.length ? (
-        spaces?.pages.map((group, i) => (
-          <Fragment key={i}>
-            {group.responses?.map((space: SpaceResBody) => (
-              <li key={space.spaceId}>
-                <Space
-                  userName={space.ownerNickName}
-                  spaceId={space.spaceId}
-                  type="Card"
-                  spaceName={space.spaceName}
-                  spaceImage={space.spaceImagePath}
-                  description={space.description}
-                  category={CATEGORIES_RENDER[space.category]}
-                  scrap={space.scrapCount}
-                  favorite={space.favoriteCount}
-                />
-              </li>
-            ))}
-          </Fragment>
-        ))
-      ) : (
-        <div className="flex justify-center text-base text-gray9">
-          {NONE_SEARCH_RESULT}
-        </div>
-      )}
+  return isSpacesLoading ? (
+    <Spinner />
+  ) : (
+    <>
+      <ul className="flex flex-col gap-y-2 px-4 pt-2">
+        {spaces?.pages[0].responses.length
+          ? spaces?.pages.map((group, i) => (
+              <Fragment key={i}>
+                {group.responses?.map((space: SpaceResBody) => (
+                  <li key={space.spaceId}>
+                    <Space
+                      userName={space.ownerNickName}
+                      spaceId={space.spaceId}
+                      type="Card"
+                      spaceName={space.spaceName}
+                      spaceImage={space.spaceImagePath}
+                      description={space.description}
+                      category={CATEGORIES_RENDER[space.category]}
+                      scrap={space.scrapCount}
+                      favorite={space.favoriteCount}
+                    />
+                  </li>
+                ))}
+              </Fragment>
+            ))
+          : !isSpacesLoading && (
+              <div className="flex justify-center text-base text-gray9">
+                {NONE_SEARCH_RESULT}
+              </div>
+            )}
 
-      <div ref={target}></div>
-    </ul>
+        <div ref={target}></div>
+      </ul>
+    </>
   )
 }
 
