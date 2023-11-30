08d5a8f116162d3e76bf7d88b3e977308e40ac1c
Yeongjun Kim <kyjun519@gmail.com>
2023-12-01 00:18:50 +0900

    chore - devlop 충돌 해결

--- src/components/common/Sidebar/Sidebar.tsx
+++ src/components/common/Sidebar/Sidebar.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { Spinner } from '@/components'
-import { Dispatch, SetStateAction, useRef, useState } from 'react'
 import { useCurrentUser } from '@/hooks/useCurrentUser'
+import { cls } from '@/utils'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import { ArchiveBoxIcon, StarIcon } from '@heroicons/react/24/solid'
 import Link from 'next/link'
@@ -64,45 +64,20 @@ const Sidebar = ({ isSidebarOpen, onClose }: SidebarProps) => {
             isSideBarLoading ? (
               <Spinner />
             ) : (
-            <>
-              <div className="flex items-center px-2">
-                <Avatar
-                  src={currentUser.profileImagePath}
-                  width={40}
-                  height={40}
-                  alt={currentUser.nickname}
-                />
-                <p className="font-sm ml-3 w-full overflow-hidden text-ellipsis	whitespace-nowrap font-medium text-gray9">
-                  {currentUser.nickname}
-                </p>
-                <Button onClick={handleCloseClick}>
-                  <XMarkIcon className="h-6 w-6" />
-                </Button>
-              </div>
-              <Link
-                href={`/user/${currentUser.memberId}`}
-                className="my-2 px-2 py-2 text-base font-bold text-gray9"
-                onClick={onClose}>
-                프로필
-              </Link>
-              <div className="border-y border-slate3 px-2">
-                <div className="mt-2 flex justify-between py-2 text-base font-bold text-gray9">
-                  {spaceType}
-                  {spaceType === '내 스페이스' ? (
-                    <Button
-                      className="button button-round button-white"
-                      onClick={handleSpaceType}>
-                      <StarIcon className="h-4 w-4 text-yellow-300" />
-                      즐겨찾기
-                    </Button>
-                  ) : (
-                    <Button
-                      className="button button-round button-white"
-                      onClick={handleSpaceType}>
-                      <ArchiveBoxIcon className="h-4 w-4 text-emerald-500" />내
-                      스페이스
-                    </Button>
-                  )}
+              <>
+                <div className="flex items-center px-2">
+                  <Avatar
+                    src={currentUser.profileImagePath}
+                    width={40}
+                    height={40}
+                    alt={currentUser.nickname}
+                  />
+                  <p className="font-sm ml-3 w-full overflow-hidden text-ellipsis	whitespace-nowrap font-medium text-gray9">
+                    {currentUser.nickname}
+                  </p>
+                  <Button onClick={handleCloseClick}>
+                    <XMarkIcon className="h-6 w-6" />
+                  </Button>
                 </div>
                 <Link
                   href={`/user/${currentUser.memberId}`}
@@ -129,34 +104,60 @@ const Sidebar = ({ isSidebarOpen, onClose }: SidebarProps) => {
                       </Button>
                     )}
                   </div>
-                  <ul>
-                    {spaces &&
-                      Object.values(spaces).map(({ spaceId, spaceName }) => (
-                        <li
-                          className="border-b border-slate3 last:border-none"
-                          key={spaceId}>
-                          <Link
-                            href={`/space/${spaceId}`}
-                            className="block px-3 py-2.5 text-sm text-gray9"
-                            onClick={onClose}>
-                            {spaceName}
-                          </Link>
-                        </li>
-                      ))}
-                  </ul>
                   <Link
-                    href={`/user/${currentUser.memberId}/space`}
-                    className="font-gray6 my-2 block w-full rounded-3xl border border-slate6 px-3 py-2.5 text-center text-sm font-medium text-slate6"
+                    href={`/user/${currentUser.memberId}`}
+                    className="my-2 px-2 py-2 text-base font-bold text-gray9"
                     onClick={onClose}>
-                    스페이스 전체보기
+                    프로필
+                  </Link>
+                  <div className="border-y border-slate3 px-2">
+                    <div className="mt-2 flex justify-between py-2 text-base font-bold text-gray9">
+                      {spaceType}
+                      {spaceType === '내 스페이스' ? (
+                        <Button
+                          className="button button-round button-white"
+                          onClick={handleSpaceType}>
+                          <StarIcon className="h-4 w-4 text-yellow-300" />
+                          즐겨찾기
+                        </Button>
+                      ) : (
+                        <Button
+                          className="button button-round button-white"
+                          onClick={handleSpaceType}>
+                          <ArchiveBoxIcon className="h-4 w-4 text-emerald-500" />
+                          내 스페이스
+                        </Button>
+                      )}
+                    </div>
+                    <ul>
+                      {spaces &&
+                        Object.values(spaces).map(({ spaceId, spaceName }) => (
+                          <li
+                            className="border-b border-slate3 last:border-none"
+                            key={spaceId}>
+                            <Link
+                              href={`/space/${spaceId}`}
+                              className="block px-3 py-2.5 text-sm text-gray9"
+                              onClick={onClose}>
+                              {spaceName}
+                            </Link>
+                          </li>
+                        ))}
+                    </ul>
+                    <Link
+                      href={`/user/${currentUser.memberId}/space`}
+                      className="font-gray6 my-2 block w-full rounded-3xl border border-slate6 px-3 py-2.5 text-center text-sm font-medium text-slate6"
+                      onClick={onClose}>
+                      스페이스 전체보기
+                    </Link>
+                  </div>
+                  <Link
+                    href="/space/create"
+                    className="my-2 px-2 py-2 text-base font-bold text-gray9"
+                    onClick={onClose}>
+                    스페이스 생성
                   </Link>
                 </div>
-                <Link
-                  href="/space/create"
-                  className="my-2 px-2 py-2 text-base font-bold text-gray9"
-                  onClick={onClose}>
-                  스페이스 생성
-                </Link>
               </>
             )
           ) : (
