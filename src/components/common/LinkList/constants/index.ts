3cc2524fb8aafc07920b4365ee7f7b587f6692e7
Yeongjun Kim <kyjun519@gmail.com>
2023-12-01 18:28:32 +0900

    feat: #209 - 링크 생성, 수정 폼 validation 텍스트 수정

--- src/components/common/LinkList/constants/index.ts
+++ src/components/common/LinkList/constants/index.ts
@@ -18,6 +18,6 @@ export const LINK_FORM_VALIDATION = {
   INCORRECT_URL: '올바르지 않은 URL입니다.',
   URL_NOT_BUTTTON: 'URL 입력 후 확인 버튼을 눌러주세요.',
   NONE_TITLE: '제목을 입력해 주세요.',
-  TITLE_LENGTH: '제목은 2글자 이상 50글자 이하로 작성해야 합니다.',
-  TAG_LENGTH: '태그는 10글자 이하로 작성해야 합니다.',
+  TITLE_LENGTH: '제목은 2자 이상 50자 이하로 작성해야 합니다.',
+  TAG_LENGTH: '태그는 10자 이하로 작성해야 합니다.',
 }
