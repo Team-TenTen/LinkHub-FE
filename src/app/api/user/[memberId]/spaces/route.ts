f78408975680324f892a2ffc221128b529ad6729
Yeongjun Kim <kyjun519@gmail.com>
2023-12-01 18:10:12 +0900

    feat: #205 - 프로필 스페이스 검색 api에 토큰 추가

--- src/app/api/user/[memberId]/spaces/route.ts
+++ src/app/api/user/[memberId]/spaces/route.ts
@@ -1,3 +1,4 @@
+import { useServerCookie } from '@/hooks/useServerCookie'
 import { apiServer } from '@/services/apiServices'
 import { NextRequest, NextResponse } from 'next/server'
 
@@ -5,12 +6,16 @@ export async function GET(
   req: NextRequest,
   { params }: { params: { memberId: number } },
 ) {
+  const { token } = useServerCookie()
   const { searchParams } = new URL(req.url)
   const memberId = params.memberId
   const path = `/members/${memberId}/spaces/search`
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  }
 
   try {
-    const data = await apiServer.get(`${path}?${searchParams}`, {})
+    const data = await apiServer.get(`${path}?${searchParams}`, {}, headers)
     return NextResponse.json(data)
   } catch (error: any) {
     return NextResponse.json(
