68bb4c8a79a1721ad3252c53a9f02059e73c7a5e
eeseung <seunghee9941@gmail.com>
2023-12-03 03:40:18 +0900

    refactor: #221 - 알림 페이지 공통 레이아웃 client component 분리

--- src/app/(routes)/notification/layout.tsx
+++ src/app/(routes)/notification/layout.tsx
@@ -1,27 +1,7 @@
-'use client'
-
-import Tab from '@/components/common/Tab/Tab'
-import TabItem from '@/components/common/Tab/TabItem'
-import useTab from '@/components/common/Tab/hooks/useTab'
+import { NotificationController } from '@/components'
 
 const NotificationLayout = ({ children }: { children: React.ReactNode }) => {
-  const { currentTab, tabList } = useTab({ type: 'notification' })
-
-  return (
-    <>
-      <Tab>
-        {tabList.map((tabItem) => (
-          <TabItem
-            active={currentTab === tabItem.content}
-            text={tabItem.text}
-            dest={tabItem.dest}
-            key={tabItem.content}
-          />
-        ))}
-      </Tab>
-      <div className="p-4">{children}</div>
-    </>
-  )
+  return <NotificationController>{children}</NotificationController>
 }
 
 export default NotificationLayout
