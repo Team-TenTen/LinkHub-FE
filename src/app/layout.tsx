299ba7235ca544825b6126483fd63dbb8ee71d19
eeseung <seunghee9941@gmail.com>
2023-12-03 17:17:27 +0900

    feat: #221 - meta 태그 maximum-scale 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       <head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, user-scalable=no"
+          content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0"
         />
       </head>
       <TanstackQueryContext>
