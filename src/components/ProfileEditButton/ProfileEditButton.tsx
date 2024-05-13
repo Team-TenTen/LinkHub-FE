a48685c92990d6aa78e398fcc6647346170d3602
Yeongjun Kim <kyjun519@gmail.com>
2024-05-13 14:43:36 +0900

    feat: #301 - profile 수정 버튼을 클라이언트 컴포넌트로 분리

---
+++ src/components/ProfileEditButton/ProfileEditButton.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import { useFollowUser, useModal } from '@/hooks'
+import { useCurrentUser } from '@/hooks/useCurrentUser'
+import { UserProfileResBody } from '@/types'
+import { cls, getProfileButtonColor, getProfileButtonText } from '@/utils'
+import { useRouter } from 'next/navigation'
+import Button from '../common/Button/Button'
+import Spinner from '../common/Spinner/Spinner'
+
+const ProfileEditButton = ({ user }: { user: UserProfileResBody }) => {
+  const router = useRouter()
+  const { currentUser } = useCurrentUser()
+  const myId = currentUser?.memberId
+  const { handleOpenCurrentModal } = useModal()
+  const { isFollowing, handleClickFollow } = useFollowUser({
+    memberId: user?.memberId || 0,
+    isInitFollowing: !!user?.isFollowing,
+    followingInitCount: user?.followingCount || 0,
+    followerInitCount: user?.followerCount || 0,
+    handleOpenCurrentModal,
+  })
+
+  return myId ? (
+    <Button
+      type="button"
+      onClick={() => {
+        if (user?.memberId === myId) {
+          router.push('/user/setting')
+        } else if (isFollowing) {
+          handleClickFollow(isFollowing)
+        } else {
+          handleClickFollow(isFollowing)
+        }
+      }}
+      className={cls(
+        'button button-md button-lg',
+        getProfileButtonColor({
+          isFollowing,
+          memberId: user?.memberId,
+          myId,
+        }),
+      )}>
+      {getProfileButtonText({
+        isFollowing,
+        memberId: user?.memberId,
+        myId,
+      })}
+    </Button>
+  ) : (
+    <Spinner />
+  )
+}
+
+export default ProfileEditButton
