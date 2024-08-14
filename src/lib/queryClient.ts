633151ff87fa51039e914cc1246e22de8339cc3f
B._.OMI <krshlee22@naver.com>
2024-08-14 14:35:25 +0900

    feat: 324 - query생성 유틸함수 추가

--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -1,5 +1,31 @@
-import { cache } from 'react'
-import { QueryClient } from '@tanstack/react-query'
+import {
+  QueryClient,
+  defaultShouldDehydrateQuery,
+  isServer,
+} from '@tanstack/react-query'
 
-const getQueryClient = cache(() => new QueryClient())
-export default getQueryClient
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+      dehydrate: {
+        shouldDehydrateQuery: (query) =>
+          defaultShouldDehydrateQuery(query) ||
+          query.state.status === 'pending',
+      },
+    },
+  })
+}
+
+let browserQueryClient: QueryClient | undefined = undefined
+
+export function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient()
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient()
+    return browserQueryClient
+  }
+}
