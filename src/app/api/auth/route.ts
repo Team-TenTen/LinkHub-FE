03ea27ead6cc77d3fd7b007adf1ac0596d328b78
Yeongjun Kim <kyjun519@gmail.com>
2025-03-15 12:48:52 +0900

    refactor: API 라우터 경로 변경

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,17 +1,21 @@
 import { useServerCookie } from '@/hooks/useServerCookie'
-import { apiServer } from '@/services/apiServices'
 import { NextResponse } from 'next/server'
+
+const baseURL = process.env.NEXT_PUBLIC_API_ADDRESS
 
 export async function GET(_request: Request) {
   const { token } = useServerCookie()
+  const path = `${baseURL}/members/profile`
+
   try {
-    const response = await apiServer.get(`/members/profile`, {
+    const response = await fetch(path, {
+      method: 'GET',
       headers: {
         Authorization: 'Bearer ' + token,
       },
     })
-
-    return NextResponse.json(response)
+    const data = await response.json()
+    return NextResponse.json(data)
   } catch (error: any) {
     return NextResponse.json(
       { error: error.response.data.message },
